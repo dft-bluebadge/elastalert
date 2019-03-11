0407c1525d71de966b5fdf31a4424dc9b4a4c46f
root <root@ip-172-22-1-101.localdomain>
2019-03-11 15:08:29 +0000

    BBK-326 | include the startup script

---
+++ elastalert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /opt/elastalert
+
+docker run -d -p 3030:3030 \
+    -v `pwd`/config/elastalert.yaml:/opt/elastalert/config.yaml \
+    -v `pwd`/config/elastalert-test.yaml:/opt/elastalert/config-test.yaml \
+    -v `pwd`/config/config.json:/opt/elastalert-server/config/config.json \
+    -v `pwd`/elastalert_modules:/opt/elastalert/elastalert_modules \
+    -v `pwd`/rules:/opt/elastalert/rules \
+    -v `pwd`/rule_templates:/opt/elastalert/rule_templates \
+    --net="host" \
+    --name elastalert bitsensor/elastalert:latest
