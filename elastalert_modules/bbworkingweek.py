89c6c1607a5f513cc824ceab5806eeba0fbf334e
root <root@ip-172-22-1-101.localdomain>
2019-03-12 10:35:58 +0000

    BBK-326 | 24 hour clock, mcfly!

--- elastalert_modules/bbworkingweek.py
+++ elastalert_modules/bbworkingweek.py
@@ -3,10 +3,10 @@ import datetime
 
 class BBWorkingWeek(BaseEnhancement):
     def process(self, match):
-        # no alerts on saturday or sunday
+        # no alerts at the weekend or out of hours mon-fri
         if datetime.datetime.now().weekday() in [5, 6]:
             raise DropMatchException
-        if datetime.datetime.now().hour < 8:
+        if datetime.datetime.now().hour < 9:
             raise DropMatchException
-        if datetime.datetime.now().hour > 5:
+        if datetime.datetime.now().hour > 17:
             raise DropMatchException
